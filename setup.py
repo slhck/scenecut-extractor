46aefb47c3a85af94ce50e55509773c655730f5b
Werner Robitza <werner.robitza@gmail.com>
2020-03-15 17:01:05 +0100

    python 3.7 and 3.8

--- setup.py
+++ setup.py
@@ -40,6 +40,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
     packages=['scenecut_extractor'],
     entry_points={
